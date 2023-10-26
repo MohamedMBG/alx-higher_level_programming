f601442ed64d5e8249cee66c6d519e195c1860da
MohamedMBG <123114744+MohamedMBG@users.noreply.github.com>
2023-10-26 19:08:50 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,52 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
+
+// Base URL for the Star Wars API
+const baseURL = 'https://swapi.dev/api/';
+
+// Check if movie ID is provided
+if (!process.argv[2]) {
+    console.error('Please provide a Movie ID.');
+    process.exit(1);
+}
+
+// Construct the URL for the film
+const filmURL = `${baseURL}films/${process.argv[2]}/`;
+
+request(filmURL, (error, response, body) => {
+    if (error) {
+        console.error('Error fetching the film:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('Invalid status code:', response.statusCode);
+        return;
+    }
+
+    const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
-  }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+function printCharacters(characters, index) {
+    if (index >= characters.length) {
+        return; // End of the recursion
     }
-  });
+
+    request(characters[index], (error, response, body) => {
+        if (error) {
+            console.error('Error fetching the character:', error);
+            return;
+        }
+
+        if (response.statusCode !== 200) {
+            console.error('Invalid status code for character:', response.statusCode);
+            return;
+        }
+
+        console.log(JSON.parse(body).name);
+        // Recursive call for the next character
+        printCharacters(characters, index + 1);
+    });
 }
