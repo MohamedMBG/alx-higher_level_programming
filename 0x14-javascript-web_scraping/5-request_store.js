539034189dd4dab5110457402fc1f5e69d8c73d4
MohamedMBG <123114744+MohamedMBG@users.noreply.github.com>
2023-10-26 19:09:09 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,10 +5,20 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
+if (!url || !filePath) {
+    console.error('Usage: ./5-request_store.js <url> <filePath>');
+    process.exit(1);
+}
+
+request(url, (err, response, body) => {
+    if (err) {
+        console.error('Error fetching the URL:', err);
+        return;
+    }
+
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+        if (err) {
+            console.error('Error writing to the file:', err);
+        }
+    });
 });
