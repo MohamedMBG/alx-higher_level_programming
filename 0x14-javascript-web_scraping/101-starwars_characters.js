a9cb615430b7f02911c744872304f0d49dea58d2
MohamedMBG <123114744+MohamedMBG@users.noreply.github.com>
2023-10-26 19:19:00 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,52 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-// Base URL for the Star Wars API
-const baseURL = 'https://swapi.dev/api/';
-
-// Check if movie ID is provided
-if (!process.argv[2]) {
-    console.error('Please provide a Movie ID.');
-    process.exit(1);
-}
-
-// Construct the URL for the film
-const filmURL = `${baseURL}films/${process.argv[2]}/`;
-
-request(filmURL, (error, response, body) => {
+function helpRequest (arr, i) {
+  if (i === arr.length) {
+    return;
+  }
+  request(arr[i], function (error, response, body) {
     if (error) {
-        console.error('Error fetching the film:', error);
-        return;
+      console.log(error);
     }
-
-    if (response.statusCode !== 200) {
-        console.error('Invalid status code:', response.statusCode);
-        return;
-    }
-
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-});
-
-function printCharacters(characters, index) {
-    if (index >= characters.length) {
-        return; // End of the recursion
-    }
-
-    request(characters[index], (error, response, body) => {
-        if (error) {
-            console.error('Error fetching the character:', error);
-            return;
-        }
-
-        if (response.statusCode !== 200) {
-            console.error('Invalid status code for character:', response.statusCode);
-            return;
-        }
-
-        console.log(JSON.parse(body).name);
-        // Recursive call for the next character
-        printCharacters(characters, index + 1);
-    });
+    console.log(JSON.parse(body).name);
+    helpRequest(arr, i + 1);
+  });
 }
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const charac = JSON.parse(body).characters;
+  helpRequest(charac, 0);
+});
