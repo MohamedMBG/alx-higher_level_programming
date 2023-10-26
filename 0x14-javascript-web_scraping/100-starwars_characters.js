81959fe93471b1fe57e4a0a760597481ebf2465d
MohamedMBG <123114744+MohamedMBG@users.noreply.github.com>
2023-10-26 19:05:30 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,46 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+
+// Base URL of the Star Wars API
+const baseURL = 'https://swapi.dev/api/';
+
+// Check if movie ID is provided
+if (!process.argv[2]) {
+  console.error('Please provide a Movie ID.');
+  process.exit(1);
+}
+
+// Construct the URL for the film
+const filmURL = `${baseURL}films/${process.argv[2]}/`;
+
+request(filmURL, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching the film:', error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+
+  // Fetch details for each character
+  characters.forEach(characterURL => {
+    request(characterURL, (error, response, body) => {
+      if (error) {
+        console.error('Error fetching the character:', error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error('Invalid status code for character:', response.statusCode);
+        return;
+      }
+
+      console.log(JSON.parse(body).name);
+    });
+  });
 });
